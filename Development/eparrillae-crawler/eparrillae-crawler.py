a8a27be30ab225cd4369f2b1d91909f9d743b95b
eparrillae <eparrillae@gmail.com>
2011-12-06 21:22:35 +0100

    Updated script to include recursive navigation and storage of links in a list for further display

--- Development/eparrillae-crawler/eparrillae-crawler.py
+++ Development/eparrillae-crawler/eparrillae-crawler.py
@@ -21,14 +21,51 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# Run example: $./eparrillae-crawler.py -n 2 http://www.google.com
+
 import urllib2
 import argparse
 from BeautifulSoup import BeautifulSoup as Soup
 
-# === read url and depth level from the command line...
+#
+# recursive method for navigating through the urls
+#
+def processUrl(url, level, counter):   
 
+    totalLinks = 0
+    urlList.append(counter * " " + url)
+    while counter < level:
+
+        # === wget the target web page...
+        user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en - US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
+        page = urllib2.build_opener()
+        page.addheaders = [('User - agent', user_agent)]
+        raw_html = page.open(url).read()
+        
+        # === get the links in this web page...
+        counter += 1
+        soup_code = Soup(raw_html)
+        links = [link ['href'] for link in soup_code.findAll('a')
+        if link.has_key('href')]
+        for x in links:
+            totalLinks += 1
+            # absolute paths...
+            if x.startswith("http"): 
+                processUrl(x, level, counter)
+            # relative paths...
+            else:
+                absurl = url + x
+                processUrl(absurl, level, counter)
+    return totalLinks
+
+##
+# Main
+#
+##
+
+# === read url and depth level from the command line...
 try:
-    parser = argparse.ArgumentParser(description = "This is a simple web crawler written in Python")
+    parser = argparse.ArgumentParser(description = "This is my web crawler written in Python for the MSWL")
     parser.add_argument('url', nargs = 1 , help = 'Target URL to be crawled')
     parser.add_argument('-n', '--level', type = int, default = 1 , help = 'Depth recursive level to be reached by the crawler')
 
@@ -43,34 +80,19 @@ except AttributeError:
     print "\nERROR: Input parameters are not correct, please use --help for details about script usage" 
     exit(0)
 
-# === wget the target web page...
-
+# === parse input html and store links in a list...
 try:
-    user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en - US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
-    page = urllib2.build_opener()
-    page.addheaders = [('User - agent', user_agent)]
-    raw_html = page.open(url).read()
-
-    print "\n Raw HTML retrieved is: ", raw_html
- 
+    urlList = []
+    total = processUrl(url, level, 1)
+    print "\nThe total amount of links processed is: ", total
+    print "\nThe list of links processed is: \n"
+    for string in urlList:
+        print string
+    
 except urllib2.URLError:
     print "\nERROR: Could not open target url, please check web site is online and available using 'wget' command!" 
     exit(0)
 
-# === parse input html and store links recursively...
 
-soup_code = Soup(raw_html)
-links = [link ['href'] for link
-                         in soup_code.findAll('a')
-                         if link.has_key('href')]
 
-print "\n The list of links is:"
-for i in links:
-    print i
     		 
-# hacer una funcion que reciba una url y lea sus links
-# la llamamos de forma recursiva, en cada paso le sumamos
-# nos salimos de un bucle infinito cuando se haya alcanzado el nivel
-# identamos la salida
-# comprobar que las urls estan bien, quitar enlaces con javascript etc... hay que quitarlos
-# comprobar que comienzan con http
